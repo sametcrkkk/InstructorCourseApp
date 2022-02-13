7f08d274d087fe7374f71d1c1109fcdb598d9c99
Samet <sametcoruk0604@gmail.com>
2022-02-13 11:42:07 +0300

    İnsructor course app.

---
+++ src/main/java/com/bilgeadam/instructorcourseapp/InstructorCourseAppApplication.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.instructorcourseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorCourseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstructorCourseAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bilgeadam/instructorcourseapp/InstructorCourseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.instructorcourseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructorCourseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
